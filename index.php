d6b7337abe58aa065db5f7daa1b12c2f622c7668
bessonnet <charles.bessonnet91@orange.fr>
2013-11-28 16:44:13 +0100

    modify the front page
    - change index.php

--- index.php
+++ index.php
@@ -1,15 +1,188 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+
+<html class="no-js" lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+<meta charset="utf-8">
+<title> Hackaton </title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Hackaton</title>
+<link rel="stylesheet" href="css/copycss.css">
+
+
+<!-- This site is optimized with the Yoast WordPress SEO plugin v1.4.18 - http://yoast.com/wordpress/seo/ -->
+<meta name="description" content="Roots is a WordPress starter theme based on HTML5 Boilerplate &amp; Bootstrap. Modernize your WordPress development workflow."/>
+<link rel="canonical" href="http://roots.io/" />
+<link rel="author" href="https://plus.google.com/110694421149047248513"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="article"/>
+<meta property="og:title" content="Roots | HTML5 WordPress Starter Theme"/>
+<meta property="og:description" content="Roots is a WordPress starter theme based on HTML5 Boilerplate &amp; Bootstrap. Modernize your WordPress development workflow."/>
+<meta property="og:url" content="http://roots.io/"/>
+<meta property="og:site_name" content="Roots"/>
+<meta property="og:image" content="http://roots.io/media/roots.png"/>
+<meta name="twitter:card" content="summary"/>
+<meta name="twitter:site" content="@retlehs"/>
+<meta name="twitter:domain" content="Roots"/>
+<meta name="twitter:creator" content="@retlehs"/>
+<!-- / Yoast WordPress SEO plugin. -->
+
+
+<link rel="alternate" type="application/rss+xml" title="Roots Feed" href="/feed/">
 </head>
+<body class="home page">
 
-<body>
-	Bonjour
+<header class="banner" role="banner">
+<div class="container">
+<div itemscope itemtype="http://schema.org/Organization">
+<a class="brand" itemprop="url" href="/">
+<img itemprop="logo" src="http://roots.io/assets/img/roots.svg" class="hidden">
+<i class="icon-roots"></i>
+<b>Roots</b>
+</a>
+</div>
+
+<nav class="nav-main" role="navigation">
+<ul id="menu-primary-navigation" class="nav"><li class="menu-docs"><a href="/docs/">Docs</a></li>
+<li class="menu-themes"><a href="/themes/">Themes</a></li>
+<li class="menu-screencasts"><a href="/screencasts/">Screencasts</a></li>
+<li class="menu-plugins"><a href="/plugins/">Plugins</a></li>
+<li class="menu-blog"><a href="/blog/">Blog</a></li>
+<li class="menu-community"><a href="/community/">Community</a></li>
+</ul>    </nav>
+</div>
+</header>
+<div class="wrap " role="document">
+<div class="content ">
+<main class="main col-lg-12" role="main">
+<section class="intro">
+<div class="container">
+<h1>Hackaton</h1>
+<h2>Travel, travel, travel ... </h2>
+<ul class="list-inline list-btns">
+<li><a class="btn btn-primary btn-lg" href="/docs/">Create a Room</a></li>
+<li></li>
+</ul>
+</div>
+
+</section>
+<section class="share">
+<div class="container">
+<ul class="list-share">
+<li><a class="btn" href="https://github.com/JonathanSenky/hackaton-jomajo"><i class="icon-github"></i> GitHub</a></li>
+<li><a class="btn" href="http://docs.outpost.travel/en/latest/"><i class="icon-github"></i> Outpost.Travel</a></li>
+</ul>
+</div>
+</section>
+
+
+
+<section class="testimonials">
+<div class="container">
+<h2>Hackaton ?</h2>
+<div class="row">
+<div class="col-sm-6">
+<blockquote class="t-1">
+<p>From the beginning, Roots just made sense to me. Logical code organization and everything seemed to be in its place. It's just a great consistent base that I've worked into to the workflow here at our agency.</p>
+<small><img src="http://www.gravatar.com/avatar/d2e16050be7de854854eb724f0de2d67?s=80&d=mm&r=g" width="45" height="45" class="img-circle"> <a href="http://www.konemarketing.com/">Dan Egan</a></small>
+</blockquote>
+</div>
+<div class="col-sm-6">
+<blockquote class="t-2">
+<p>Roots extends and builds on what drew me to WordPress: the ability to create simple and functional sites very quickly, with the power to build custom and complex functionality as well.</p>
+<small><img src="http://www.gravatar.com/avatar/5b7205324676b483047d491bbe8adca6?s=80&d=mm&r=g" width="45" height="45" class="img-circle"> <a href="http://c2itllc.com/">Chris Carr</a></small>
+</blockquote>
+
+<div class="row">
+<div class="col-lg-12">
+<blockquote class="t-3">
+<p>Your theme is one of the best Bootstrap based starter themes. But to me your theme is more than that, it helped me, inspired me, and changed my way of organizing project structure. I've learned many new things via GitHub discussions too. There was a hard learning curve in the beginning compared to other WP themes, but when you pass that stage everything is just right. I have my own freedom to do anything with your theme. I really appreciate all developers who worked hard making this theme.</p>
+<small><img src="http://www.gravatar.com/avatar/b1bcef210fbb8c637928f7212140ed33?s=80&d=mm&r=g" width="45" height="45" class="img-circle"> <a href="http://simonalex.com/">Alex Lim</a></small>
+</blockquote>
+</div>
+</div>
+</div>
+</section>
+
+<section class="gallery">
+<div class="container">
+<h2>
+Built with Roots
+<a class="btn btn-primary" href="/gallery/">View Gallery</a>
+</h2>
+<div class="gallery-featured row">
+<div class="gallery-item col-xs-6 col-md-3">
+<a href="/gallery/otherinbox/" class="thumbnail"><img src="/media/otherinbox.jpg" alt="OtherInbox" title="OtherInbox"></a>
+</div>
+<div class="gallery-item col-xs-6 col-md-3">
+<a href="/gallery/webink-blog/" class="thumbnail"><img src="/media/webink-blog.jpg" alt="WebINK Blog" title="WebINK Blog"></a>
+</div>
+<div class="gallery-item col-xs-6 col-md-3">
+<a href="/gallery/shoretel-sky/" class="thumbnail"><img src="/media/shoretel-sky.jpg?112013" alt="ShoreTel Sky" title="ShoreTel Sky"></a>
+</div>
+<div class="gallery-item col-xs-6 col-md-3">
+<a href="/gallery/women-and-tech/" class="thumbnail"><img src="/media/women-and-tech.jpg" alt="Women And Tech" title="Women And Tech"></a>
+</div>
+</div>
+</div>
+</section>
+
+
+
+
+
+
+
+</main>
+</div>
+</div>
+
+<footer class="content-info" role="contentinfo">
+<div class="container">
+<nav class="nav-footer" role="navigation">
+<ul id="menu-primary-navigation-1" class=""><li class="menu-docs"><a href="/docs/">Docs</a></li>
+<li class="menu-themes"><a href="/themes/">Themes</a></li>
+<li class="menu-screencasts"><a href="/screencasts/">Screencasts</a></li>
+<li class="menu-plugins"><a href="/plugins/">Plugins</a></li>
+<li class="menu-blog"><a href="/blog/">Blog</a></li>
+<li class="menu-community"><a href="/community/">Community</a></li>
+</ul>      <ul>
+<li><a href="/about/">About</a></li>
+<li><a href="/gallery/">Gallery</a></li>
+<li><a href="http://demo.roots.io/">Demo</a></li>
+</ul>
+<ul>
+<li><a href="https://github.com/roots/roots"><i class="icon-github"></i></a></li>
+<li><a href="https://twitter.com/retlehs"><i class="icon-twitter"></i></a></li>
+</ul>
+</nav>
+<p>&copy; 2013 Roots &middot; <a href="/privacy/">Privacy Policy</a></p>
+<p><i class="icon-roots"></i></p>
+</div>
+</footer>
+
+<script src="/assets/js/1f7070be.scripts.min.js"></script>
+
+
+<script>
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+                                                        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+ e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+ e.src='//www.google-analytics.com/analytics.js';
+ r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-71591-42');ga('send','pageview');
+
+var clicky_site_id = 66406668;
+(function() {
+ var s = document.createElement('script');
+ s.type = 'text/javascript';
+ s.async = true;
+ s.src = '//static.getclicky.com/js';
+ ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+ })();
+</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66406668ns.gif"></p></noscript>
+
 </body>
 </html>
-
-
