83b7b0093983e4a931b777808d326fabe1c0c9b8
Matthieu NICOLAS <matthieu.nicolas@telecomnancy.net>
2013-11-28 18:33:45 +0100

    Modification de la base de données + implémentation d'un jeu d'essai + services/s_commentaires.php

---
+++ services/s_commentaires.php
@@ -0,0 +1,54 @@
+<?php
+    include_once('../connexion_db/connexion.php');
+
+    if(isset($_GET['mode']) && !empty($_GET['mode']))
+    {
+        if(isset($_GET['idSalle']) && is_numeric($_GET['idSalle']))
+        {
+            $mode = $_GET['mode'];
+            $idSalle = $_GET['idSalle'];
+            
+            if($mode=='get')
+            {
+                recup_commentaires($idSalle);
+            }
+            else if($mode=='post')
+            {
+                if(isset($_GET['auteur']) && !empty($_GET['auteur']))
+                {
+                    if(isset($_GET['contenu']) && !empty($_GET['contenu']))
+                    {
+                        $auteur = $_GET['auteur'];
+                        $contenu = $_GET['contenu'];
+                        post_commentaire($idSalle, $auteur, $contenu);
+                    }
+                }
+            }
+        }
+    }
+    
+    function recup_commentaires($idSalle)
+    {
+        global $bdd;
+        $req = $bdd->prepare('SELECT idCommentaire, auteur, contenu, dateCommentaire FROM commentaires WHERE idSalle=?');
+        $req->execute(array($idSalle));
+        $commentaires = array();
+        while($row=$req->fetch())
+        {
+            array_push($commentaires, array('auteur'=>$row[1], 'contenu'=>$row[2], 'dateCommentaire'=>$row[3]));
+        }
+        $req->closeCursor();
+        print_r($commentaires);
+        echo '<br>';
+        echo json_encode($commentaires);
+    }
+    
+    function post_commentaire($idSalle, $auteur, $contenu)
+    {
+        global $bdd;
+        $req = $bdd->prepare('INSERT INTO Commentaires VALUES (NULL, ?, ?, ?, ?);');
+        $req->execute(array($idSalle, $auteur, $contenu, date('Y-m-d H:i:s')));
+        $req->closeCursor();
+    }
+    
+?>
--- teamkog_hackaton_2013.sql
+++ teamkog_hackaton_2013.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.8
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
--- Client: sql427.redheberg.com
--- Généré le: Jeu 28 Novembre 2013 à 15:02
--- Version du serveur: 5.1.69-log
--- Version de PHP: 5.3.17
+-- Client: localhost
+-- Généré le: Jeu 28 Novembre 2013 à 17:30
+-- Version du serveur: 5.5.24-log
+-- Version de PHP: 5.3.13
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -23,26 +23,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Commentaires`
+-- Structure de la table `commentaires`
 --
 
-CREATE TABLE IF NOT EXISTS `Commentaires` (
+CREATE TABLE IF NOT EXISTS `commentaires` (
   `idcommentaire` int(10) NOT NULL AUTO_INCREMENT,
   `idSalle` int(5) NOT NULL,
   `auteur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `contenu` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `dateCommentaire` date NOT NULL,
+  `dateCommentaire` datetime NOT NULL,
   PRIMARY KEY (`idcommentaire`),
   KEY `idSalle` (`idSalle`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `commentaires`
+--
+
+INSERT INTO `commentaires` (`idcommentaire`, `idSalle`, `auteur`, `contenu`, `dateCommentaire`) VALUES
+(1, 1, 'Matt', 'Coucou', '0000-00-00 00:00:00'),
+(2, 1, 'Matt', 'Coucou', '0000-00-00 00:00:00'),
+(3, 1, 'Matt', 'Coucou', '0000-00-00 00:00:00'),
+(4, 1, 'Matt', 'Coucou', '2013-11-28 16:34:59'),
+(5, 1, 'Matt', 'Coucou2', '2013-11-28 16:37:58');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Etapes`
+-- Structure de la table `etapes`
 --
 
-CREATE TABLE IF NOT EXISTS `Etapes` (
+CREATE TABLE IF NOT EXISTS `etapes` (
   `idEtape` int(5) NOT NULL AUTO_INCREMENT,
   `idSalle` int(10) NOT NULL,
   `destination` varchar(50) CHARACTER SET latin1 NOT NULL,
@@ -50,74 +61,95 @@ CREATE TABLE IF NOT EXISTS `Etapes` (
   `dateFin` date NOT NULL,
   PRIMARY KEY (`idEtape`),
   KEY `idSalle` (`idSalle`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `etapes`
+--
+
+INSERT INTO `etapes` (`idEtape`, `idSalle`, `destination`, `dateDebut`, `dateFin`) VALUES
+(1, 1, 'Paris', '2013-11-28', '2013-11-29');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Propositions`
+-- Structure de la table `propositions`
 --
 
-CREATE TABLE IF NOT EXISTS `Propositions` (
-  `idProposition` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `propositions` (
+  `idProposition` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `idEtape` int(5) NOT NULL,
   `idTypeProposition` int(3) NOT NULL,
   `votePlus` int(5) NOT NULL,
-  `voteMoins` int(5) NOT NULL,
-  PRIMARY KEY (`idProposition`),
+  PRIMARY KEY (`idProposition`,`idEtape`,`idTypeProposition`),
   KEY `idEtape` (`idEtape`),
   KEY `idTypeProposition` (`idTypeProposition`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Salles`
+-- Structure de la table `salles`
 --
 
-CREATE TABLE IF NOT EXISTS `Salles` (
+CREATE TABLE IF NOT EXISTS `salles` (
   `idSalle` int(5) NOT NULL AUTO_INCREMENT,
   `nomSalle` varchar(50) CHARACTER SET latin1 NOT NULL,
   `lienSalle` varchar(200) CHARACTER SET latin1 NOT NULL,
   `dateCreation` date NOT NULL,
   `dateFin` date NOT NULL,
   PRIMARY KEY (`idSalle`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `salles`
+--
+
+INSERT INTO `salles` (`idSalle`, `nomSalle`, `lienSalle`, `dateCreation`, `dateFin`) VALUES
+(1, 'Essai', 'tructructruc', '2013-11-28', '2013-11-29');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `TypesPropositions`
+-- Structure de la table `typespropositions`
 --
 
-CREATE TABLE IF NOT EXISTS `TypesPropositions` (
+CREATE TABLE IF NOT EXISTS `typespropositions` (
   `idTypeProposition` int(3) NOT NULL AUTO_INCREMENT,
   `libelleProposition` varchar(50) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`idTypeProposition`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `typespropositions`
+--
+
+INSERT INTO `typespropositions` (`idTypeProposition`, `libelleProposition`) VALUES
+(1, 'Logement'),
+(2, 'Expérience');
 
 --
 -- Contraintes pour les tables exportées
 --
 
 --
--- Contraintes pour la table `Commentaires`
+-- Contraintes pour la table `commentaires`
 --
-ALTER TABLE `Commentaires`
-  ADD CONSTRAINT `Commentaires_ibfk_1` FOREIGN KEY (`idSalle`) REFERENCES `Salles` (`idSalle`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `commentaires`
+  ADD CONSTRAINT `Commentaires_ibfk_1` FOREIGN KEY (`idSalle`) REFERENCES `salles` (`idSalle`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `Etapes`
+-- Contraintes pour la table `etapes`
 --
-ALTER TABLE `Etapes`
-  ADD CONSTRAINT `Etapes_ibfk_1` FOREIGN KEY (`idSalle`) REFERENCES `Salles` (`idSalle`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `etapes`
+  ADD CONSTRAINT `Etapes_ibfk_1` FOREIGN KEY (`idSalle`) REFERENCES `salles` (`idSalle`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `Propositions`
+-- Contraintes pour la table `propositions`
 --
-ALTER TABLE `Propositions`
-  ADD CONSTRAINT `Propositions_ibfk_2` FOREIGN KEY (`idTypeProposition`) REFERENCES `TypesPropositions` (`idTypeProposition`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Propositions_ibfk_1` FOREIGN KEY (`idEtape`) REFERENCES `Etapes` (`idEtape`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `propositions`
+  ADD CONSTRAINT `Propositions_ibfk_1` FOREIGN KEY (`idEtape`) REFERENCES `etapes` (`idEtape`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Propositions_ibfk_2` FOREIGN KEY (`idTypeProposition`) REFERENCES `typespropositions` (`idTypeProposition`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
