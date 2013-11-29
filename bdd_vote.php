4d6865aeb09b244833f6fe0d6ed459c78f72c83d
Matthieu NICOLAS <matthieu.nicolas@telecomnancy.net>
2013-11-29 09:21:05 +0100

    Complétion de bdd_vote.php

--- bdd_vote.php
+++ bdd_vote.php
@@ -1,27 +1,33 @@
 <?php
-
-// recuperation par get de la cle ( 3 champ)
-    $idProposition      = $_GET['idProposition']
-    $idEtape            = $_GET['idEtape']
-    $idTypeProposition  = $_GET['idTypeProposition']
-//verifie si la ligne exisst dna la BD
+    // recuperation par get de la cle ( 3 champ)
+    $idProposition = $_GET['idProposition'];
+    $idEtape = $_GET['idEtape'];
+    $idTypeProposition = $_GET['idTypeProposition'];
     
-    //includes
-	include_once("fonctions.php");
-	include_once("connexion_db/connexion.php");
+    //verifie si la ligne existe dans la BD
     
-    $req_verif = $bdd->prepare("SELECT COUNT (*) FROM propositons WHERE 'idProposition'=$idProposition and 'idEtape'=$idEtape" and'idTypeProposition'= $idTypeProposition);
-    $req_verif->execute();
-    $row=$req_verif->fetch();
-
+    //include
+    include_once('connexion_db/connexion.php');
     
-    if($row[0]!=0){
+    $req = $bdd->prepare('SELECT votePlus FROM propositions WHERE idProposition=? and idEtape=? and idTypeProposition=?');
+    $req->execute(array($idProposition, $idEtape, $idTypeProposition));
+    echo $req->rowCount();
+    if($req->rowCount()>0)
+    {
         //set la valeur de plus a set +1
-        
-    }else{
-        //insert la nouvelle ligne
+        $row=$req->fetch();
+        echo $row[0];
+        $votePlus = $row[0]+1;
+        $req_update = $bdd->prepare('UPDATE propositions SET votePlus=? WHERE idProposition=? and idEtape=? and idTypeProposition=?');
+        $req_update->execute(array($votePlus, $idProposition, $idEtape, $idTypeProposition));
+        $req_update->closeCursor();
     }
-    
-
-	
+    else
+    {
+        //insert la nouvelle ligne
+        $req_insert = $bdd->prepare('INSERT INTO propositions VALUES (?, ?, ?, ?)');
+        $req_insert->execute(array($idProposition, $idEtape, $idTypeProposition, 1));
+        $req_insert->closeCursor();
+    }
+    $req->closeCursor();	
 ?>
